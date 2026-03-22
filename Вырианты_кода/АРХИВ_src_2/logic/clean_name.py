92318333fc53ef65c26c392ea8c02b60ca77e296
Alexey Alexey <alexey28.01.1989@gmail.com>
2026-03-22 19:42:06 +0300

    22.03.26 19:41 Убрал из репозитория залетевший мусор и делаю финальный коммит: Fix: apply final gitignore and remove archive from tracking

--- Вырианты_кода/АРХИВ_src_2/logic/clean_name.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def clean_name(name):
-    
-    pattern = r'^([^/\s]+)'
-    
-    match = re.match(pattern, name)
-    if match:
-        return match.group(1)
--- Вырианты_кода/АРХИВ_src_2/logic/clean_price.py
+++
@@ -1,8 +0,0 @@
-
-def clean_price(price):
-    if not price: return 0
-    # Просто убираем ВСЁ, кроме цифр и точки
-    clean_val = price.replace('$', '').replace(',', '') 
-    return float(clean_val)
-
-# print(clean_price('$34,45,245,452,452,4'))
