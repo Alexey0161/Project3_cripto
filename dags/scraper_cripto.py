e7adc8a35493d4ac7fc1f4f6121cb42f1acb4e6d
Alexey Alexey <alexey28.01.1989@gmail.com>
2026-04-02 15:11:07 +0300

    02.04.26 15:09 Внеск коррективы в файл docker_compose.yaml добавил строки в раздел окружение, которые устраняют конфликт запроса адресов из контейнера

--- dags/scraper_cripto.py
+++ dags/scraper_cripto.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "libs"))
-from datetime import datetime, timedelta  # ИСПРАВЛЕНО: добавили timedelta
+from datetime import datetime, timedelta  
 
 # Настраиваем пути, чтобы видеть папку logic
 current_dir = os.path.dirname(os.path.abspath(__file__))
